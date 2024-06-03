90ca77e047dbaa8bd4dba6e338b75512ce436ed5
Ahmed Hassan <ah2k.dev@gmail.com>
2024-06-03 22:31:24 +0500

    Update wix.js

--- src/router/wix.js
+++ src/router/wix.js
@@ -3,5 +3,6 @@ import wix from "../controllers/wix.js";
 const router = express.Router();
 
 router.post("/file", wix.createFile);
+router.post("/business", wix.createBusiness);
 
 export default router;
