279c7c71b8d37ba7c5c191374ad0dcf5547a84c2
Ahmed Hassan <ah2k.dev@gmail.com>
2024-06-03 19:25:42 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import http from "http";
 // dotenv.config({ path: "./src/config/config.env" }); //load env vars
 
 //server setup
-const PORT = process.env.PORT || 8003;
+const PORT = process.env.PORT || 8001;
 
 var server = http.createServer(app);
 server.listen(PORT, () => {
